74c91083e327751d271c47e2363633a7a5d51fb6
James Cline <jacli697@learner.dvusd.org>
2024-01-09 12:48:41 -0700

    Changes To Menu

--- Menus/Menus/Form1.vb
+++ Menus/Menus/Form1.vb
@@ -7,4 +7,18 @@
     Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
         RichTextBox1.Clear()
     End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim OutFile As New IO.StreamWriter("Out.txt")
+        OutFile.WriteLine(RichTextBox1.Text)
+        OutFile.Close()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If IO.File.Exists("Out.txt") Then
+            Dim InFile As New IO.StreamReader("Out.txt")
+            RichTextBox1.Text = InFile.ReadToEnd()
+            InFile.Close()
+        End If
+    End Sub
 End Class
